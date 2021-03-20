96a2d4c87bdcb8c019a4a31f2e75641ac3ad1fca
Bruno Rodrigues <brunohrsilva1@gmail.com>
2021-03-20 15:29:43 -0300

    Add filters and rendering todos

--- todos.js
+++ todos.js
@@ -1,38 +1,54 @@
 const todos = [
 	{ title: 'Order cat food', completed: true },
-	{ title: 'Clean kitchen', completed: false },
+	{ title: 'Clean the kitchen', completed: false },
 	{ title: 'Buy food', completed: true },
 	{ title: 'Do work', completed: false },
 	{ title: 'Exercise', completed: true },
+	{ title: 'Walk the dog', completed: true },
 ];
 
-const filterRemainingTodos = (todos) => {
-	return todos.filter((todo) => {
-		const isCompleted = todo.completed;
+const filters = {
+	searchText: '',
+};
 
-		return !isCompleted;
+// Rendering todos on screen
+const renderTodos = (todos, filters) => {
+	const allTodos = document.querySelector('#todos');
+
+	const filteredTodos = todos.filter(({ title }) => {
+		return title.toLowerCase().includes(filters.searchText.toLowerCase());
+	});
+
+	// Check todos's length for summary text
+	const incompleteTodos = filteredTodos.filter((todo) => !todo.completed);
+
+	allTodos.innerHTML = '';
+
+	const summary = document.createElement('h2');
+	summary.textContent = `You have ${incompleteTodos.length} todos left`;
+	allTodos.appendChild(summary);
+
+	filteredTodos.forEach(({ title }) => {
+		const todoEl = document.createElement('p');
+		todoEl.textContent = title;
+		allTodos.appendChild(todoEl);
 	});
 };
 
-const sortTodos = (todos) => {
-	return todos.sort((a, b) => {
-		const A = a.completed;
-		const B = b.completed;
+renderTodos(todos, filters);
 
-		return !A && B ? -1 : !B && A ? 1 : 0;
-	});
+const insertTodo = document.querySelector('#insert-todo');
+const addTodo = document.querySelector('#add-todo');
+const filterTodo = document.querySelector('#filter-todo');
+
+// Listen for todo text change
+insertTodo.oninput = (e) => console.log(e.target.value);
+
+// Listen for add a todo to the list
+addTodo.onclick = () => console.log('Add a new todo...');
+
+// Listen for todo text change to filter it
+filterTodo.oninput = (e) => {
+	filters.searchText = e.target.value;
+	renderTodos(todos, filters);
 };
-
-const deleteTodo = (todos, title) => {
-	const index = todos.findIndex((todo) => {
-		return todo.title.toLowerCase() === title.toLowerCase();
-	});
-
-	if (index > -1) {
-		todos.splice(index, 1);
-	}
-};
-
-filterRemainingTodos(todos);
-sortTodos(todos);
-deleteTodo(todos, 'Buy food');
