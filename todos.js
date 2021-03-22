27bb59ab28818856fff5ecb1dffe0b80c5497144
Bruno Rodrigues <brunohrsilva1@gmail.com>
2021-03-21 23:33:58 -0300

    Add filter todos rendering and hide completed

--- todos.js
+++ todos.js
@@ -9,14 +9,20 @@ const todos = [
 
 const filters = {
 	searchText: '',
+	hideCompleted: false,
 };
 
 // Rendering todos on screen
 const renderTodos = (todos, filters) => {
 	const allTodos = document.querySelector('#todos');
 
-	const filteredTodos = todos.filter(({ title }) => {
-		return title.toLowerCase().includes(filters.searchText.toLowerCase());
+	let filteredTodos = todos.filter(({ title, completed }) => {
+		const searchTextMatch = title
+			.toLowerCase()
+			.includes(filters.searchText.toLowerCase());
+		const hideCompletedMatch = !filters.hideCompleted || !completed;
+
+		return searchTextMatch && hideCompletedMatch;
 	});
 
 	// Check todos's length for summary text
@@ -37,18 +43,34 @@ const renderTodos = (todos, filters) => {
 
 renderTodos(todos, filters);
 
-const insertTodo = document.querySelector('#insert-todo');
-const addTodo = document.querySelector('#add-todo');
-const filterTodo = document.querySelector('#filter-todo');
-
-// Listen for todo text change
-insertTodo.oninput = (e) => console.log(e.target.value);
-
-// Listen for add a todo to the list
-addTodo.onclick = () => console.log('Add a new todo...');
+const insertTodo = (todo) => {
+	todos.push({
+		title: todo,
+		completed: false,
+	});
+};
 
 // Listen for todo text change to filter it
+const filterTodo = document.querySelector('#filter-todo');
 filterTodo.oninput = (e) => {
 	filters.searchText = e.target.value;
+	renderTodos(todos, filters);
+};
+
+// Listen for hide completed todos
+const hideCompleted = document.querySelector('#hide-completed');
+hideCompleted.onchange = (e) => {
+	filters.hideCompleted = e.target.checked;
+	renderTodos(todos, filters);
+};
+
+// Inserting a new todo
+const newTodo = document.querySelector('#new-todo');
+newTodo.onsubmit = (e) => {
+	e.preventDefault();
+
+	insertTodo(e.target.elements.text.value);
+	e.target.elements.text.value = '';
+
 	renderTodos(todos, filters);
 };
